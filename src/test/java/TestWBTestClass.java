0d7d4c3ce3da2f05e2db6f3a688727ddee767fe5
danhorea2010 <danhorea2010@gmail.com>
2022-03-29 17:23:18 +0300

    new test class

---
+++ src/test/java/TestWBTestClass.java
@@ -0,0 +1,85 @@
+import domain.Tema;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+import repository.NotaXMLRepo;
+import repository.StudentXMLRepo;
+import repository.TemaXMLRepo;
+import service.Service;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.ValidationException;
+
+public class TestWBTestClass {
+
+    public static Service service;
+
+
+    @BeforeAll
+    public static void setup() {
+        StudentValidator studentValidator = new StudentValidator();
+        TemaValidator temaValidator = new TemaValidator();
+        String filenameStudent = "fisiere/Studenti.xml";
+        String filenameTema = "fisiere/Teme.xml";
+        String filenameNota = "fisiere/Note.xml";
+
+        StudentXMLRepo studentXMLRepository = new StudentXMLRepo(filenameStudent);
+        TemaXMLRepo temaXMLRepository = new TemaXMLRepo(filenameTema);
+        NotaValidator notaValidator = new NotaValidator(studentXMLRepository, temaXMLRepository);
+        NotaXMLRepo notaXMLRepository = new NotaXMLRepo(filenameNota);
+        TestWBTestClass.service = new Service(studentXMLRepository, studentValidator, temaXMLRepository, temaValidator, notaXMLRepository, notaValidator);
+    }
+
+    // Assignment
+    //    private String nrTema;
+    //    private String descriere;
+    //    private int deadline;
+    //    private int primire;
+
+    @Test
+    public void addTema_ValidData_CreatedSuccessfully() {
+
+        String nrTema = "Test Assignemnt";
+        String descriere = "Assignment for testing";
+        int deadline = 12;
+        int primire = 2;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(true);
+
+        }catch (ValidationException ve){
+            System.out.println("Validation Exception: " + ve.getMessage());
+            assert(false);
+
+        }
+
+    }
+
+    @Test
+    public void addTema_InValidData_CreatedSuccessfully() {
+
+        String nrTema = "";
+        String descriere = "Assignment for testing";
+        int deadline = 12;
+        int primire = 2;
+
+        Tema tema = new Tema(nrTema, descriere, deadline, primire );
+
+        try{
+            service.addTema(tema);
+            assert(false);
+
+        }catch (ValidationException ve){
+            System.out.println("Validation Exception: " + ve.getMessage());
+            assert(true);
+
+        }
+
+    }
+
+
+
+}
