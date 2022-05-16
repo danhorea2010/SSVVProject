239946981cceda2e2743817ea7c037a27ecca8a7
danhorea2010 <danhorea2010@gmail.com>
2022-05-16 15:53:48 +0300

    first commit lab5 take-home

--- lab5/2022PrjSerenityJUnit/src/test/java/ssvv/features/search/SearchByKeywordStory.java
+++ lab5/2022PrjSerenityJUnit/src/test/java/ssvv/features/search/SearchByKeywordStory.java
@@ -10,20 +10,20 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.chrome.ChromeDriver;
 import ssvv.steps.serenity.EndUserSteps;
 
 @RunWith(SerenityRunner.class)
 public class SearchByKeywordStory {
-
     @Managed(uniqueSession = true)
     public WebDriver webdriver;
 
     @Steps
     public EndUserSteps anna;
-
     @Issue("#WIKI-1")
     @Test
     public void searching_by_keyword_apple_should_display_the_corresponding_article() {
+
         anna.is_the_home_page();
         anna.looks_for("apple");
        anna.should_see_definition("A common, round fruit produced by the tree Malus domestica, cultivated in temperate climates.");
